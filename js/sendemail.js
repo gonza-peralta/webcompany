b3418945e0737c02a17f95eab112f02a623b47d8
Gonza <peraltag@gmail.com>
2016-05-08 10:54:28 -0300

    Remove sendmail option

--- js/sendemail.js
+++
@@ -1,8 +0,0 @@
-$('#submit').click(function(){
-    $(location).attr('href', 'mailto:peraltag@gmail.com?subject='
-                             + encodeURIComponent("This is my subject")
-                             + "&body=" 
-                             + encodeURIComponent("This is my body")
-    );
-    /*alert( "Handler for .click() called." );*/
-});
